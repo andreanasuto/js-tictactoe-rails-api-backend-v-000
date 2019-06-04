f1c1e7b3f1d5a84e7ecaac6f8c7dcaa421050dbc
Andrea Nasuto <andrea.nasuto@gmail.com>
2019-06-04 21:29:39 +0000

    Automatically backed up by Learn

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,4 +1,20 @@
 class GamesController < ApplicationController
   # Add your GamesController code here
+  def create
+    @game = Game.new(state: params[:state])
+    @game.save
+  end
+
+  def show
+    @game = Game.find(params[:id])
+  end
+
+  def update
+
+  end
+
+  def index
+
+  end
 
 end
